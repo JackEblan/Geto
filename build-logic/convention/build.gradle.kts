cb70c9381d3f634e84b4d1820a1b56bd2ac3aead
TeinTraveler <teinocnalb@gmail.com>
2023-11-26 07:03:49 +0800

    Updated build-logic

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -6,6 +6,11 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
 
 dependencies {
     compileOnly(libs.android.gradle.plugin)
--- build-logic/convention/src/main/kotlin/com/android/geto/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/android/geto/KotlinAndroid.kt
@@ -3,6 +3,7 @@ package com.android.geto
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.provideDelegate
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -22,9 +23,20 @@ internal fun Project.configureKotlinAndroid(
         }
     }
 
+    configureKotlin()
+}
+
+private fun Project.configureKotlin() {
     tasks.withType<KotlinCompile>().configureEach {
         kotlinOptions {
             jvmTarget = JavaVersion.VERSION_17.toString()
+            val warningsAsErrors: String? by project
+            allWarningsAsErrors = warningsAsErrors.toBoolean()
+            freeCompilerArgs = freeCompilerArgs + listOf(
+                "-opt-in=kotlin.RequiresOptIn",
+                "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+                "-opt-in=kotlinx.coroutines.FlowPreview",
+            )
         }
     }
 }
