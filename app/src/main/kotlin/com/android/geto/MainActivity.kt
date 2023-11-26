1787e8eccaa37fa88688f6f186e9a6d1b2bafad3
TeinTraveler <teinocnalb@gmail.com>
2023-11-27 06:26:54 +0800

    -Removed the toast when app is launched -Added fastlane -Updated gradle version

--- app/src/main/kotlin/com/android/geto/MainActivity.kt
+++ app/src/main/kotlin/com/android/geto/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.android.geto
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -21,13 +20,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        Toast.makeText(
-            applicationContext,
-            "Test app developed by Jack Eblan. The app will be slower in debug mode.",
-            Toast.LENGTH_SHORT
-        ).show()
-
         setContent {
             DevOpsHideNoRootTheme {
                 Surface(
