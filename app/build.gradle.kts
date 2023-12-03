e198f1a5d7a77b7ab0ff60b2c5556a14c7e9c6d1
TeinTraveler <teinocnalb@gmail.com>
2023-12-03 11:17:33 +0800

    Updated to version 1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
 
     defaultConfig {
         applicationId = "com.android.geto"
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 4
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
