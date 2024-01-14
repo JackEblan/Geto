f9a40b11755d6f4441248e4165b4be3cbef60365
JackEblan <jackeblan115@gmail.com>
2024-01-14 16:33:19 +0800

    Added AndroidApplicationFirebaseConventionPlugin

---
+++ build-logic/convention/src/main/kotlin/AndroidApplicationFirebaseConventionPlugin.kt
@@ -0,0 +1,36 @@
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.geto.libs
+import com.google.firebase.crashlytics.buildtools.gradle.CrashlyticsExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidApplicationFirebaseConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply(libs.plugins.gms.get().pluginId)
+                apply(libs.plugins.firebase.crashlytics.get().pluginId)
+            }
+
+            dependencies {
+                val bom = libs.firebase.bom
+                add("implementation", platform(bom))
+                add("implementation", libs.firebase.analytics)
+                add("implementation", libs.firebase.crashlytics)
+            }
+
+            extensions.configure<ApplicationExtension> {
+                buildTypes.configureEach {
+                    // Disable the Crashlytics mapping file upload. This feature should only be
+                    // enabled if a Firebase backend is available and configured in
+                    // google-services.json.
+                    configure<CrashlyticsExtension> {
+                        mappingFileUploadEnabled = false
+                    }
+                }
+            }
+        }
+    }
+}
