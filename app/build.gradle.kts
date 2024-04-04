9b6eabfd673fd6742d0a0e5841b5175afbd849af
JackEblan <jackeblan115@gmail.com>
2024-04-04 13:46:38 +0800

    Fix cannot find GetoTestRunner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,10 +91,13 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
     testImplementation(libs.androidx.navigation.testing)
-    testImplementation(projects.core.dataTest)
-    testImplementation(projects.core.testing)
     testImplementation(libs.hilt.android.testing)
     testImplementation(libs.robolectric)
+    testImplementation(projects.core.dataTest)
+    testImplementation(projects.core.testing)
+        
+    androidTestImplementation(libs.hilt.android.testing)
+    androidTestImplementation(projects.core.testing)
 
     baselineProfile(projects.benchmarks)
 }
