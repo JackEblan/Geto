e2001a73594968ed14829a1d6c6b13dfe178e189
JackEblan <jackeblan115@gmail.com>
2024-12-04 05:59:05 +0800

    Added UI to Shizuku attempt 1

--- feature/permission/src/main/kotlin/com/android/geto/feature/permission/PermissionScreen.kt
+++ feature/permission/src/main/kotlin/com/android/geto/feature/permission/PermissionScreen.kt
@@ -22,31 +22,177 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.android.geto.core.designsystem.component.AnimatedWavyCircle
+import com.android.geto.core.designsystem.icon.GetoIcons
+import com.android.geto.core.domain.model.ShizukuStatus
 
 @Composable
 internal fun PermissionRoute(
     modifier: Modifier = Modifier,
     viewModel: PermissionViewModel = hiltViewModel(),
+    onNavigationIconClick: () -> Unit,
 ) {
+    val shizukuStatus by viewModel.shizukuStatus.collectAsStateWithLifecycle()
+
+    val snackbarHostState = remember {
+        SnackbarHostState()
+    }
+
     PermissionScreen(
         modifier = modifier,
+        snackbarHostState = snackbarHostState,
+        shizukuStatus = shizukuStatus,
+        onEvent = viewModel::onEvent,
+        onNavigationIconClick = onNavigationIconClick,
     )
 }
 
 @Composable
-internal fun PermissionScreen(modifier: Modifier = Modifier) {
-    Scaffold { innerPadding ->
+internal fun PermissionScreen(
+    modifier: Modifier = Modifier,
+    snackbarHostState: SnackbarHostState,
+    shizukuStatus: ShizukuStatus,
+    onEvent: (PermissionEvent) -> Unit,
+    onNavigationIconClick: () -> Unit,
+) {
+    var isBound by rememberSaveable { mutableStateOf(false) }
+
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    DisposableEffect(key1 = lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_CREATE) {
+                onEvent(PermissionEvent.OnCreate)
+            } else if (event == Lifecycle.Event.ON_DESTROY) {
+                onEvent(PermissionEvent.OnDestroy)
+            }
+        }
+
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+
+    LaunchedEffect(key1 = shizukuStatus) {
+        when (shizukuStatus) {
+            ShizukuStatus.UnBound -> {
+                isBound = false
+            }
+
+            ShizukuStatus.Bound -> {
+            }
+
+            ShizukuStatus.Granted -> {
+                snackbarHostState.showSnackbar(message = "Shizuku Permission Granted")
+            }
+
+            ShizukuStatus.Denied -> {
+                snackbarHostState.showSnackbar(message = "Shizuku Permission Denied")
+            }
+
+            ShizukuStatus.UpgradeShizuku -> {
+                snackbarHostState.showSnackbar(message = "Please upgrade Shizuku version")
+            }
+
+            ShizukuStatus.CanWriteSecureSettings -> {
+                isBound = true
+
+                snackbarHostState.showSnackbar(message = "Write Secure Settings granted")
+            }
+
+            ShizukuStatus.RemoteException -> {
+                snackbarHostState.showSnackbar(message = "Something went wrong")
+            }
+
+            ShizukuStatus.DeadBinder -> {
+                snackbarHostState.showSnackbar(message = "Shizuku Service not running")
+            }
+
+            ShizukuStatus.AliveBinder -> {
+                snackbarHostState.showSnackbar(message = "Shizuku Service running")
+            }
+
+            ShizukuStatus.Loading -> {
+                snackbarHostState.showSnackbar(message = "Connecting to Shizuku")
+            }
+        }
+    }
+
+    Scaffold(
+        topBar = {
+            AppSettingsTopAppBar(
+                title = "Permission",
+                onNavigationIconClick = onNavigationIconClick,
+            )
+        },
+        snackbarHost = {
+            SnackbarHost(
+                hostState = snackbarHostState,
+                modifier = Modifier.testTag("appSettings:snackbar"),
+            )
+        },
+    ) { innerPadding ->
         Box(
             modifier = modifier
                 .padding(innerPadding)
                 .fillMaxSize()
                 .consumeWindowInsets(innerPadding),
         ) {
-
+            AnimatedWavyCircle(
+                modifier = modifier.fillMaxSize(),
+                active = isBound,
+                onClick = {
+                    onEvent(PermissionEvent.CheckShizukuPermission)
+                },
+            )
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun AppSettingsTopAppBar(
+    modifier: Modifier = Modifier,
+    title: String,
+    onNavigationIconClick: () -> Unit,
+) {
+    TopAppBar(
+        title = {
+            Text(text = title, maxLines = 1)
+        },
+        modifier = modifier.testTag("permission:topAppBar"),
+        navigationIcon = {
+            IconButton(onClick = onNavigationIconClick) {
+                Icon(
+                    imageVector = GetoIcons.Back,
+                    contentDescription = "Navigation icon",
+                )
+            }
+        },
+    )
 }
--- framework/shizuku/src/main/kotlin/com/android/geto/framework/shizuku/DefaultShizukuWrapper.kt
+++ framework/shizuku/src/main/kotlin/com/android/geto/framework/shizuku/DefaultShizukuWrapper.kt
@@ -26,7 +26,11 @@ import android.content.pm.PackageManager
 import android.os.IBinder
 import android.os.RemoteException
 import com.android.geto.core.domain.framework.ShizukuWrapper
+import com.android.geto.core.domain.model.ShizukuStatus
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import rikka.shizuku.Shizuku
 import rikka.shizuku.Shizuku.OnRequestPermissionResultListener
 import javax.inject.Inject
@@ -37,65 +41,156 @@ class DefaultShizukuWrapper @Inject constructor(@ApplicationContext private val 
     private val requestPermissionResult = 1
 
     private val requestPermissionResultListener =
-        OnRequestPermissionResultListener { requestCode, grantResult ->
+        OnRequestPermissionResultListener { _, grantResult ->
+            if (grantResult == PackageManager.PERMISSION_GRANTED) {
+                _shizukuStatus.tryEmit(
+                    ShizukuStatus.Granted,
+                )
+            } else {
+                _shizukuStatus.tryEmit(
+                    ShizukuStatus.Denied,
+                )
+            }
         }
 
     private var userService: IUserService? = null
+
+    private val serviceArgs = Shizuku.UserServiceArgs(
+        ComponentName(
+            context.packageName,
+            UserService::class.java.getName(),
+        ),
+    ).processNameSuffix("user_service").debuggable(BuildConfig.DEBUG)
+
+    private val _shizukuStatus =
+        MutableSharedFlow<ShizukuStatus>(replay = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
+
+    override val shizukuStatus = _shizukuStatus.asSharedFlow()
+
+    private var _isBinderAlive = false
+
+    private var _isBound = false
 
     private val connection = object : ServiceConnection {
         override fun onServiceConnected(componentName: ComponentName, binder: IBinder?) {
             if (binder != null && binder.pingBinder()) {
                 userService = IUserService.Stub.asInterface(binder)
 
-                try {
-                    userService?.grantRuntimePermission(
-                        context.packageName,
-                        Manifest.permission.WRITE_SECURE_SETTINGS,
-                        0,
-                    )
-                } catch (e: RemoteException) {
-                    e.printStackTrace()
-                }
+                _isBound = true
+
+                grantRuntimePermission()
             }
         }
 
         override fun onServiceDisconnected(componentName: ComponentName) {
-            userService = null;
+            userService = null
+
+            _isBound = false
         }
     }
 
+    private val onBinderReceivedListener = Shizuku.OnBinderReceivedListener {
+        _isBinderAlive = true
+
+        _shizukuStatus.tryEmit(
+            ShizukuStatus.AliveBinder,
+        )
+    }
+
+    private val onBinderDeadListener = Shizuku.OnBinderDeadListener {
+        _isBinderAlive = false
+
+        _shizukuStatus.tryEmit(
+            ShizukuStatus.DeadBinder,
+        )
+    }
+
     override fun onCreate() {
+        Shizuku.addBinderReceivedListener(onBinderReceivedListener)
+
+        Shizuku.addBinderDeadListener(onBinderDeadListener)
+
         Shizuku.addRequestPermissionResultListener(requestPermissionResultListener)
     }
 
     override fun onDestroy() {
         Shizuku.removeRequestPermissionResultListener(requestPermissionResultListener)
+
+        Shizuku.removeBinderReceivedListener(onBinderReceivedListener)
+
+        Shizuku.removeBinderDeadListener(onBinderDeadListener)
     }
 
-    override fun checkPermission(): Boolean {
-        if (Shizuku.isPreV11()) {
-            return false
+    override fun checkShizukuPermission() {
+        if (_isBinderAlive.not()) {
+            _shizukuStatus.tryEmit(
+                ShizukuStatus.DeadBinder,
+            )
+            return
         }
 
-        if (Shizuku.checkSelfPermission() == PackageManager.PERMISSION_GRANTED) {
-            if (Shizuku.getVersion() >= 10) {
-                val serviceArgs = Shizuku.UserServiceArgs(
-                    ComponentName(
-                        context.packageName,
-                        UserService::class.java.getName(),
-                    ),
-                ).processNameSuffix("user_service").debuggable(BuildConfig.DEBUG)
+        if (Shizuku.isPreV11()) {
+            _shizukuStatus.tryEmit(
+                ShizukuStatus.Denied,
+            )
+        } else if (Shizuku.checkSelfPermission() == PackageManager.PERMISSION_GRANTED) {
+            _shizukuStatus.tryEmit(
+                ShizukuStatus.Loading,
+            )
 
-                Shizuku.bindUserService(serviceArgs, connection)
-            } else {
-                //Tell the user to upgrade Shizuku.
-            }
-            return true
+            updateUserService()
         } else if (Shizuku.shouldShowRequestPermissionRationale()) {
-            return false
+            _shizukuStatus.tryEmit(
+                ShizukuStatus.Denied,
+            )
         } else {
-            Shizuku.requestPermission(requestPermissionResult);
-            return false
+            Shizuku.requestPermission(requestPermissionResult)
+        }
+    }
+
+    private fun updateUserService() {
+        if (_isBound) {
+            unbindUserService()
+        } else {
+            bindUserService()
+        }
+    }
+
+    private fun bindUserService() {
+        if (Shizuku.getVersion() >= 10) {
+            Shizuku.bindUserService(serviceArgs, connection)
+        } else {
+            _shizukuStatus.tryEmit(
+                ShizukuStatus.UpgradeShizuku,
+            )
+        }
+    }
+
+    private fun unbindUserService() {
+        Shizuku.unbindUserService(serviceArgs, connection, true)
+
+        _isBound = false
+
+        _shizukuStatus.tryEmit(
+            ShizukuStatus.UnBound,
+        )
+    }
+
+    private fun grantRuntimePermission() {
+        try {
+            userService?.grantRuntimePermission(
+                context.packageName,
+                Manifest.permission.WRITE_SECURE_SETTINGS,
+                0,
+            )
+
+            _shizukuStatus.tryEmit(
+                ShizukuStatus.CanWriteSecureSettings,
+            )
+        } catch (e: RemoteException) {
+            _shizukuStatus.tryEmit(
+                ShizukuStatus.RemoteException,
+            )
         }
     }
 }
